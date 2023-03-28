a0c65f27c120a72fd70e4577bf37bb25f8320d22
Thibault COSATTINI <t.cosattini@gmail.com>
2023-03-28 12:31:58 +0200

    boiler plate authentification

--- app.config.ts
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  name: "expo_boiler_plate",
-  version: "1.0.0",
-  extra: {
-    API_URL_PREPROD: "https://preprod.acantech-api.fr/",
-    API_URL_LOCAL: "http://127.0.0.1:8000/",
-  },
-};
